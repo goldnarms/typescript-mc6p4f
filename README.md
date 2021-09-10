0265f4052adf052728bba450917601d72adee4c0
goldnarms <arnstej@gmail.com>
2021-09-10 09:31:36 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-mc6p4f
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-mc6p4f)
---
+++ index.ts
@@ -0,0 +1,6 @@
+import './style.css';
+import { select } from 'd3';
+
+// Bruk select og velg <div> med id 'helloWorldContainer' fra DOM'en
+
+// Deretter kan du legge til en <h1> til valgt div, med tekst "Hello World"
